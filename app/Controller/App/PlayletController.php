4d7c4fed6fa1f97b2de28e638fb346174d24d77b
ryc1008 <ryc934858@gmail.com>
2024-02-25 18:00:42 +0800

    no message

--- app/Controller/App/PlayletController.php
+++ app/Controller/App/PlayletController.php
@@ -39,12 +39,13 @@ class PlayletController extends CommonController
         $user = $this->user();
         $model = $this->model($this->m);
         foreach ($list->items() as &$item){
-            $user['is_buy'] = 1;//免费的显示
+            $user['is_buy'] = 1;//免费设置成已购买，不显示
             if($item['money'] > 0){
+                $user['is_buy'] = 0;//不免费没购买，显示
                 //必须是购买，钻石才能看
                 $buy = $this->isBuy($user['id'], $item['id'], $model);
                 if($buy){
-                    $user['is_buy'] = 0; //不免费的没购买的显示
+                    $user['is_buy'] = 1; //不免费的已购买，不显示
                 }
             }
             //是否收藏
